6eb7c120debb0613d4a4ae49ef6aed0bf49a3d3c
Emil Pausz <emilpausz@gmail.com>
2016-06-26 17:27:01 +0200

    add basic tests

--- app/app.js
+++ app/app.js
@@ -1,5 +1,75 @@
 var game = (function() {
+	var fields = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+		userFields = [],
+		pcFields = [],
+		possibleWins = [
+			// left right
+			[1,2,3], [4,5,6], [7,8,9],
+			// top bottom
+			[1,4,7], [2,5,8], [3,6,9],
+			//cross
+			[1,5,9], [3,5,7]
+		],
+		whoIsActive = 'PC';
+
+	function getUserFields(){
+		return userFields;
+	}
+
+	function getPcFields(){
+		return pcFields;
+	}
+
+	function getPossibleWins() {
+		return possibleWins;
+	}
 	
+	function checkIsEmpty(fieldId){
+		return fields.indexOf(fieldId) > -1 ? true : false;
+	}
+
+	function changePossibleWins(fieldId, activeUser){
+		activeUser = activeUser || whoIsActive;
+		possibleWins = possibleWins.map(function(arr){
+			return arr.map(function(value) {
+				return value === fieldId ? activeUser : value;
+			});
+		});
+	}
+
+	function checkWin(isUser) {
+		var isWinner = false;
+		var length = possibleWins.length;
+		whoIsActive = isUser ? 'USER' : 'PC';
+
+		for(var i = 0; i < length; i++) {
+			// if count will be 3 then somebody wins
+			var count = 0;
+			for(var j = 0; j < 3; j++) 
+				if(possibleWins[i][j] === whoIsActive)
+					count++;
+			if(count === 3) {
+				isWinner = true;
+ 				break;
+			}
+		}
+		return isWinner;
+	}
+
+	function makeMove(fieldId, isUser) {
+		whoIsActive = isUser ? 'USER' : 'PC';
+		if(!checkIsEmpty(fieldId)) 
+			return false;
+		
+		// add to user or pc fields
+		if(isUser) 
+			userFields.push(fieldId);
+		else
+			pcFields.push(fieldId);
+		// remove from available fields
+		fields.splice(fields.indexOf(fieldId), 1);
+		changePossibleWins(fieldId, whoIsActive);
+	}
 
 	function cacheDOM() {
 
@@ -15,4 +85,21 @@ var game = (function() {
 	}
 
 	init();
+	
+	return {
+		checkIsEmpty: checkIsEmpty,
+		checkWin: checkWin,
+		getUserFields: getUserFields,
+		getPcFields: getPcFields,
+		getPossibleWins: getPossibleWins,
+		makeMove: makeMove,
+		changePossibleWins: changePossibleWins
+	}
+
 })();
+
+try {
+	module.exports = game;
+} catch(e) {
+	console.log('You dont have a module system bundler');
+}
---
+++ test/game.js
@@ -0,0 +1,65 @@
+var should = require('should');
+var game = require('../app/app.js');
+
+
+describe('A Tic-tac-toe game test.', function() {
+
+	describe('Check fields', function () {
+		
+		it('should return true when value is in fields<array>.', function() {
+			game.checkIsEmpty(3).should.equal(true);
+		});
+
+		it('should return false when value is not in fields<array>.', function() {
+			game.checkIsEmpty(10).should.equal(false);
+		});
+
+	});
+
+	describe('Make a move.', function() {
+
+		describe('User and PC move.', function() {
+
+			it('should add fields[1,2,3]<number> to a userFields<array>.', function(){
+				game.makeMove(1, true);
+				game.makeMove(2, true);
+				game.makeMove(3, true);
+				game.getUserFields().should.have.length(3);
+			});
+
+			it('should add fields[7,8,4]<number> to a pcFields<array>.', function(){
+				game.makeMove(7);
+				game.makeMove(8);
+				game.makeMove(8);
+				game.makeMove(2);
+				game.makeMove(4);
+				game.makeMove(4);
+				game.getPcFields().should.have.length(3);
+			});
+
+			// it('should change every 2<number> to "USER" in every array in possibleWins<array> .', function () {
+			// 	game.changePossibleWins(2, 'USER');
+			// 	game.getPossibleWins()[0][1].should.equal('USER');
+			// 	game.getPossibleWins()[4][0].should.equal('USER');
+			// });
+
+			// it('should change every 3<number> to "PC" in every array in possibleWins<array> .', function () {
+			// 	game.changePossibleWins(3, 'PC');
+			// 	game.getPossibleWins()[0][2].should.equal('PC');
+			// 	game.getPossibleWins()[5][0].should.equal('PC');
+			// 	game.getPossibleWins()[7][0].should.equal('PC');
+			// });
+
+		});
+
+		describe('Check for win', function() {
+
+			it('should return true if somebody win.', function(){
+				game.checkWin(true).should.equal(true);
+			});
+
+		});
+
+	});
+
+});
